2e680db737ffda667b31265e8d57142426fbf94d
Sara Elsayeda <swe@saraelsa.net>
2023-07-09 12:29:20 -0400

    Add startMagicLinkEmailAuthenticationAsync method

--- src/api/models/IAuthenticationRequest.ts
+++ src/api/models/IAuthenticationRequest.ts
@@ -1,12 +1,12 @@
 export interface IAuthenticationRequest {
     auth_request_id: string;
-    auth_validation_token: string;
     user_id: string;
     timeout_in_seconds: number;
     timeout_utc_datetime: string;
 }
 
 export interface IAuthenticatorAuthenticationRequest extends IAuthenticationRequest {
+    auth_validation_token: string;
     visual_verify_value: string | null;
     push_message_sent: boolean;
     qr_code_data: string | null;
@@ -19,3 +19,5 @@ export interface IWebAuthnAuthenticationRequest {
     fido2_json_options: string;
     aa_guid: string;
 }
+
+export interface IMagicLinkEmailAuthenticationRequest extends IAuthenticationRequest {}
