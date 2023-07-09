6cdf12f84bc1a34cdd7e143643d1996a344b106b
Sara Elsayeda <swe@saraelsa.net>
2023-07-09 12:06:25 -0400

    Add startAuthenticatorAuthentication method

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,5 +1,7 @@
 export * from "./IAuthDetails";
+export * from "./IAuthenticationRequest";
 export * from "./IAuthInfo";
 export * from "./IAuthMethod";
 export * from "./IAuthTokenInfo";
+export * from "./IOriginLocationData";
 export * from "./ISecureSignedMessage";
