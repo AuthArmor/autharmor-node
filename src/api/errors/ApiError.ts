95ca64e4cdb62bbd52ebeae6ae9ec976838c5df6
saraelsa <sara-etx@outlook.com>
2023-07-09 12:29:53 -0400

    Run prettier

--- src/api/errors/ApiError.ts
+++ src/api/errors/ApiError.ts
@@ -1,8 +1,5 @@
 export class ApiError extends Error {
-    public constructor(
-        public readonly statusCode: number,
-        public readonly statusMessage: string
-    ) {
+    public constructor(public readonly statusCode: number, public readonly statusMessage: string) {
         super(`${statusCode} ${statusMessage}`);
     }
 }
