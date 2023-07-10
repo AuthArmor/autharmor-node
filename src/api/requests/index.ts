7790691844e23fe96f92c5676c19bb18dde2efef
Sara Elsayeda <swe@saraelsa.net>
2023-07-10 17:23:04 -0400

    Add startAuthenticatorUserRegistrationAsync method

--- src/api/requests/index.ts
+++ src/api/requests/index.ts
@@ -1,5 +1,6 @@
 export * from "./IFinishWebAuthnAuthenticationRequest";
 export * from "./IPagingRequest";
 export * from "./IStartAuthenticationRequest";
+export * from "./IStartAuthenticatorUserRegistrationRequest";
 export * from "./IUpdateUserRequest";
 export * from "./IValidateAuthenticationRequest";
