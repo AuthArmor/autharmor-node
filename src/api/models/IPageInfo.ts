f1f23386b67a8ddf566b3aae03e8deebce0d4a88
Sara Elsayeda <swe@saraelsa.net>
2023-07-10 14:37:16 -0400

    Add listUsersAsync endpoint

---
+++ src/api/models/IPageInfo.ts
@@ -0,0 +1,8 @@
+export interface IPageInfo {
+    // Misspelled at source.
+    currnet_page_number: number;
+    // Misspelled at source.
+    currnet_page_size: number;
+    total_page_count: number;
+    total_record_count: number;
+}
