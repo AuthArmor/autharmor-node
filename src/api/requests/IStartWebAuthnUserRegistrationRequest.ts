842c57c16ad346e4cb776d67e167c59c073110f1
Sara Elsayeda <swe@saraelsa.net>
2023-07-11 06:57:12 -0400

    Add startWebAuthnRegistrationForExistingUserAsync endpoint

--- src/api/requests/IStartWebAuthnUserRegistrationRequest.ts
+++
@@ -1,9 +0,0 @@
-export interface IStartWebAuthnUserRegistrationRequest {
-    username?: string | null;
-    emailAddress?: string | null;
-    webAuthnClientId: string;
-    webAuthnAttachmentType?: WebAuthnAttachmentType;
-    timeoutSeconds?: number | null;
-}
-
-export type WebAuthnAttachmentType = "Any" | "Platform" | "CrossPlatform";
--- src/api/requests/index.ts
+++ src/api/requests/index.ts
@@ -3,6 +3,6 @@ export * from "./IFinishWebAuthnRegistrationRequest";
 export * from "./IPagingRequest";
 export * from "./IStartAuthenticationRequest";
 export * from "./IStartAuthenticatorUserRegistrationRequest";
-export * from "./IStartWebAuthnUserRegistrationRequest";
+export * from "./IStartWebAuthnRegistrationRequest";
 export * from "./IUpdateUserRequest";
 export * from "./IValidateAuthenticationRequest";
