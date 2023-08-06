d358543670eb43f1697ba45eb690785a3c2c48f4
Sara Elsayeda <swe@saraelsa.net>
2023-08-06 17:31:33 -0400

    Update general error handling

--- src/api/errors/ApiError.ts
+++ src/api/errors/ApiError.ts
@@ -1,5 +1,15 @@
+import { IApiError } from "../models";
+
 export class ApiError extends Error {
-    public constructor(public readonly statusCode: number, public readonly statusMessage: string) {
-        super(`${statusCode} ${statusMessage}`);
+    public readonly statusCode: number;
+    public readonly message: string;
+    public readonly trackingId: string;
+
+    public constructor(public readonly rawError: IApiError) {
+        super(`[${rawError.errorCode}] ${rawError.errorMessage}`);
+
+        this.statusCode = rawError.errorCode;
+        this.message = rawError.errorMessage;
+        this.trackingId = rawError.trackingGuid;
     }
 }
---
+++ src/api/models/IApiError.ts
@@ -0,0 +1,5 @@
+export interface IApiError {
+    errorMessage: string;
+    errorCode: number;
+    trackingGuid: string;
+}
