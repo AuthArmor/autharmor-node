bf877e09dece8cd78be051d231fae29705462cf7
Sara Elsayeda <swe@saraelsa.net>
2024-05-08 12:59:28 -0400

    Add registration status code enum

--- src/api/models/IRegistrationInfo.ts
+++ src/api/models/IRegistrationInfo.ts
@@ -1,7 +1,8 @@
 import { AuthMethod } from "./IAuthMethod";
+import { RegistrationRequestStatusCode } from "./RegistrationRequestStatusCode";
 
 export interface IRegistrationInfo {
     registration_status_name: string;
-    registration_status_code: number;
+    registration_status_code: RegistrationRequestStatusCode;
     auth_method: AuthMethod;
 }
